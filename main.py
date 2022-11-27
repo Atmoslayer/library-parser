e3ad314000357b4927d10116da90bcbc67cc4188
Atmoslayer <ddsomdim@gmail.com>
2022-11-27 21:01:12 +0300

    Download books from tululu.org

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+import os
+import logging
+from requests.exceptions import HTTPError
+logging.basicConfig(level=logging.INFO)
+
+
+def save_book(book_id, folder_name):
+    url = 'https://tululu.org/txt.php'
+    params = {'id': book_id}
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    filename = f'book{book_id}.txt'
+
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+    with open(f'{folder_name}/{filename}', 'w') as file:
+        file.write(response.text)
+
+
+if __name__ == "__main__":
+    for book_id in range(10):
+        try:
+            save_book(book_id, 'Books')
+        except HTTPError as http_error:
+            print(f'HTTP error occurred: {http_error}')
+
+
+
+
+
+
