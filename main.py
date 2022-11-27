561ff2d3a5b7b95b9cad2e48a67741c261c1a6a2
Atmoslayer <ddsomdim@gmail.com>
2022-11-27 23:18:24 +0300

    Download book covers

--- main.py
+++ main.py
@@ -2,7 +2,23 @@ import requests
 import os
 import logging
 from requests.exceptions import HTTPError
+from pathvalidate import sanitize_filename
+from bs4 import BeautifulSoup
+
 logging.basicConfig(level=logging.INFO)
+
+
+def parse_book(book_id):
+    url = 'https://tululu.org/'
+    response = requests.get(f'{url}b{book_id}/')
+    check_for_redirect(response, book_id)
+    soup = BeautifulSoup(response.text, 'lxml')
+    header_tag_text = soup.find('h1').text
+    book_name, author = header_tag_text.split('::')
+    book_name = book_name.strip()
+    image_url = soup.find('div', class_='bookimage').find('img')['src']
+
+    return book_name, image_url
 
 
 def check_for_redirect(response, book_id):
@@ -10,23 +26,44 @@ def check_for_redirect(response, book_id):
         raise HTTPError(f'No book with id {book_id}')
 
 
-def save_book(book_id, folder_name):
+def download_image(image_url, book_id, folder='images'):
+    image_url_elements = os.path.splitext(image_url)
+    image_type = image_url_elements[1]
+    file_dir = f'{folder}/{book_id}{image_type}'
+    image_link = f'https://tululu.org{image_url}'
+    response = requests.get(image_link)
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    with open(f'{file_dir}', 'ab') as image:
+        image.write(response.content)
+
+
+def download_txt(book_id, book_name, folder='books'):
     url = 'https://tululu.org/txt.php'
     params = {'id': book_id}
     response = requests.get(url, params=params)
     check_for_redirect(response, book_id)
-    filename = f'book{book_id}.txt'
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
 
-    with open(f'{folder_name}/{filename}', 'w') as file:
+    pure_filename = sanitize_filename(f'{book_id}.{book_name}')
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    file_dir = f'{folder}/{pure_filename}'
+
+    with open(f'{file_dir}.txt', 'w') as file:
         file.write(response.text)
+
+    return os.path.join(file_dir)
 
 
 if __name__ == "__main__":
     for book_id in range(1, 11):
         try:
-            save_book(book_id, 'Books')
+            book_name, image_url = parse_book(book_id)
+            print(download_txt(book_id, book_name))
+            download_image(image_url, book_id)
         except HTTPError as http_error:
             print(f'HTTP error occurred: {http_error}')
 
