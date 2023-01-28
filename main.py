752be3c9634a1b70481e51a95fb1f7971d2405ae
Atmoslayer <ddsomdim@gmail.com>
2023-01-29 01:07:19 +0300

    Fix help description for images_path argument

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ def parse_book(soup):
 
     image_selector = '.bookimage img'
     image_url = soup.select_one(image_selector)['src']
-
     comments_selector = '.texts .black'
     comments_tags = soup.select(comments_selector)
     comments = [comment_tag.text for comment_tag in comments_tags if comments_tags]
@@ -88,7 +87,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser(description='Library parser')
     parser.add_argument('--books_path', help='Enter path to save books', type=str, default='books')
-    parser.add_argument('--images_path', help='Enter path to save books', type=str, default='images')
+    parser.add_argument('--images_path', help='Enter path to save images', type=str, default='images')
     parser.add_argument('--start_id', help='Enter start book id', default=1, type=int)
     parser.add_argument('--end_id', help='Enter end book id', default=10, type=int)
     arguments = parser.parse_args()
